def1a70c6dd46b5b1d899e5141f9c07995be8f8f
Sebastian Sarasti <sebitas.alejo@hotmail.com>
2022-02-23 18:32:48 -0500

    agregando build folder

---
+++ build/rpt_pos.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+start_time=$(date "+%s")
+
+
+for i in {1..2036}
+do 
+    ./naidetector /G4-e/RPT-posa/macros/Array_1/pos$i.mac
+    hadd scoring_f.root *.root
+    root -q det_reader1.C
+    mv NaIDetector_D1.dat NaIDetector_D1_pos$i.dat
+    mv NaIDetector_D1_pos$i.dat /G4-e/RPT-posa/results/Array_1
+    root -q det_reader2.C
+    mv NaIDetector_D2.dat NaIDetector_D2_pos$i.dat
+    mv NaIDetector_D2_pos$i.dat /G4-e/RPT-posa/results/Array_1
+    root -q det_reader3.C
+    mv NaIDetector_D3.dat NaIDetector_D3_pos$i.dat
+    mv NaIDetector_D3_pos$i.dat /G4-e/RPT-posa/results/Array_1
+    root -q det_reader4.C
+    
+    rm *.root
+done
+
+
+for i in {1..2036}
+do 
+    ./naidetector /G4-e/RPT-posa/macros/Array_2/pos$i.mac
+    hadd scoring_f.root *.root
+    root -q det_reader1.C
+    mv NaIDetector_D1.dat NaIDetector_D1_pos$i.dat
+    mv NaIDetector_D1_pos$i.dat /G4-e/RPT-posa/results/Array_2
+    root -q det_reader2.C
+    mv NaIDetector_D2.dat NaIDetector_D2_pos$i.dat
+    mv NaIDetector_D2_pos$i.dat /G4-e/RPT-posa/results/Array_2
+    root -q det_reader3.C
+    mv NaIDetector_D3.dat NaIDetector_D3_pos$i.dat
+    mv NaIDetector_D3_pos$i.dat /G4-e/RPT-posa/results/Array_2
+    root -q det_reader4.C
+    
+    rm *.root
+done
+
+for i in {1..2036}
+do 
+    ./naidetector /G4-e/RPT-posa/macros/Array_3/pos$i.mac
+    hadd scoring_f.root *.root
+    root -q det_reader1.C
+    mv NaIDetector_D1.dat NaIDetector_D1_pos$i.dat
+    mv NaIDetector_D1_pos$i.dat /G4-e/RPT-posa/results/Array_3
+    root -q det_reader2.C
+    mv NaIDetector_D2.dat NaIDetector_D2_pos$i.dat
+    mv NaIDetector_D2_pos$i.dat /G4-e/RPT-posa/results/Array_3
+    root -q det_reader3.C
+    mv NaIDetector_D3.dat NaIDetector_D3_pos$i.dat
+    mv NaIDetector_D3_pos$i.dat /G4-e/RPT-posa/results/Array_3
+    root -q det_reader4.C
+    
+    rm *.root
+done
+
+
+end_time=$(date "+%s")
+s=60
+let elapsed=($end_time-$start_time)/$s
+echo "$elapsed minutes"
